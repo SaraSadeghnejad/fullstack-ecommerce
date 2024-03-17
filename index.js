f52a32d0d56a59bcb1b9580d81bfc60ba5deafab
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-17 19:30:11 +0330

    backend side

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const productRouter = require("./routes/products");
+const app = express();
+const port = 3000;
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("db connected"))
+  .catch((error) => console.log("error" + error));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/api/products", productRouter);
+app.listen(process.env.PORT || port, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
